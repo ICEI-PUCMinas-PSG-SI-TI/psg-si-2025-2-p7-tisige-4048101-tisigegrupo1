8fbb9820a1fe69514997b0164b8516c4bdbd7dfd
Lucas Lima <works.lucassl@gmail.com>
2025-11-05 00:23:24 -0300

    Removendo arquivos potencialmente incorretos

--- postcss.config.cjs
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  // Use the PostCSS wrapper package for Tailwind (required for Tailwind v4+)
-    plugins: {
-      // use the dedicated PostCSS plugin bridge for Tailwind (new package)
-      '@tailwindcss/postcss': {},
-      autoprefixer: {},
-    },
-}
