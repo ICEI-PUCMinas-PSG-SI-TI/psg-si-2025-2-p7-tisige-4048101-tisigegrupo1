b3394c1fd741ca03059fae3ef2f5e59196094822
Lucas Lima <works.lucassl@gmail.com>
2025-11-06 20:08:41 -0300

    Corrigindo comentário no arquivo de config do Vite

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-   // 1. Define o caminho base (o nome do seu repositório no GitHub)
+   // 1. Define o caminho base (o nome do repositório no GitHub)
   base: "/psg-si-2025-2-p7-tisige-4048101-tisigegrupo1/",
   
   // 2. Define a pasta de saída do build como "docs"
