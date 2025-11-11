184c5ce1c24e3fd20e4009b61ad1651c7a32927d
Carlos Corvaum <101909778+carlosCorvaum@users.noreply.github.com>
2025-11-11 20:43:12 -0300

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base:"/psg-si-2025-2-p7-tisige-4048101-tisigegrupo1/",
   server: {
     host: "::",
     port: 8080,
