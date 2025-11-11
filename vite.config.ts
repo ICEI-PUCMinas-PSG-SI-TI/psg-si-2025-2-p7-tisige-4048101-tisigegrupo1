15c8b24f91d5e4b000f50c310470e0415cea0055
Carlos Corvaum <101909778+carlosCorvaum@users.noreply.github.com>
2025-11-11 20:46:48 -0300

    Modify Vite config for base path and output dir
    Updated base path and output directory for build.

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,15 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
-
+ 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base:"/psg-si-2025-2-p7-tisige-4048101-tisigegrupo1/",
+   // 1. Define o caminho base (o nome do repositório no GitHub)
+  base: "/psg-si-2025-2-p7-tisige-4048101-tisigegrupo1/",
+  // 2. Define a pasta de saída do build como "docs"
+  build: {
+    outDir: "docs",
+  },
   server: {
     host: "::",
     port: 8080,
